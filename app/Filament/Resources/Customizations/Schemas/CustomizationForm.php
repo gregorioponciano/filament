f6fe47bbae6f30f898ab0edc2e008994b90a1afb
Gregório Ponciano <gregorioponci.91@gmail.com>
2026-01-17 12:58:52 -0300

    ajustei o commit anterior

--- app/Filament/Resources/Customizations/Schemas/CustomizationForm.php
+++ app/Filament/Resources/Customizations/Schemas/CustomizationForm.php
@@ -15,9 +15,13 @@ class CustomizationForm
                 TextInput::make('nome')
                     ->required(),
                 FileUpload::make('imagem')
+                ->image()
+                ->imageEditor()
                     ->disk('public')
                     ->directory('images/customizations')
-                    ->visibility('public'),
+                    ->visibility('public')
+                    ->required()
+                    ->helperText('PNG ou JPG até 2MB'),
             ]);
     }
 }
--- app/Models/Customization.php
+++ app/Models/Customization.php
@@ -8,7 +8,7 @@ class Customization extends Model
 {
     protected $fillable = [
         'nome',
-        'image',
+        'imagem',
     ];
 
 
